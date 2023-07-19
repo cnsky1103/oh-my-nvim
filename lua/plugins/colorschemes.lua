b2599fa399bc1f04a1069d5441552b5a02d73958
cnsky1103 <55350008+cnsky1103@users.noreply.github.com>
2023-07-19 23:34:19 +0800

    disable keyword italic

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			vim.g.hardhacker_darker = 0
 			vim.g.hardhacker_hide_tilde = 1
-			vim.g.hardhacker_keyword_italic = 1
+			vim.g.hardhacker_keyword_italic = 0
 			vim.cmd("colorscheme hardhacker")
 		end,
 	},
