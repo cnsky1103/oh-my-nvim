81d8837a21246e8f1c60139fc52eaf61db52ede5
cnsky1103 <cnsky1103@gmail.com>
2023-11-23 14:00:50 +0800

    fix remap 0 and $ bug

--- lua/core.lua
+++ lua/core.lua
@@ -135,9 +135,9 @@ local function set_keymap()
 	map("n", keys.switch_session, ":SessionManager load_session<CR>", option)
 
 	-- Change mappings of 0 and $, keep '`'
-	map("n", "`", "~")
-	map("n", "0", "`")
-	map("n", "$", "0")
+	map("n", "~", "`", option)
+	map("n", "`", "0", option)
+	map("n", "0", "$", option)
 end
 
 -- Set up transparency
